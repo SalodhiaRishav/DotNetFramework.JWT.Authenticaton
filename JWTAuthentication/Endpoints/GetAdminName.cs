67e6ce7c494cd705337b13f8ac4e0faf66661db3
Rishav Salodhia <rishav.salodhia@nagarro.com>
2019-08-13 17:18:02 +0530

    added admin api

---
+++ JWTAuthentication/Endpoints/GetAdminName.cs
@@ -0,0 +1,14 @@
+﻿using ServiceStack.ServiceHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JWTAuthentication.Endpoints
+{
+    [Route("/adminname","GET")]
+    public class GetAdminName
+    {
+        
+    }
+}
---
+++ JWTAuthentication/Services/AdminService.cs
@@ -0,0 +1,17 @@
+﻿using JWTAuthentication.Endpoints;
+using ServiceStack.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JWTAuthentication.Services
+{
+    public class AdminService : Service
+    {
+        public string Get(GetAdminName request)
+        {
+            return "this is from secured admin api. you can't see this without login as admin";
+        }
+    }
+}
